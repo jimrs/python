243e65a500ebd40ecedc8aa044e1c4a29892460a
Jiri Mares <marj39@vse.cz>
2019-01-25 21:00:34 +0100

    added new game option on loss or win

--- pyladies/sibenice/hra.py
+++ pyladies/sibenice/hra.py
@@ -1,36 +1,61 @@
 import util
+import sys
 
-wordList = util.createWordList()
-word = util.pickWord(wordList)
-secret = util.createSecret(word)
-badGuessCount = 0
+while True:     # new game loop
 
-while True:
-    guess = input("INPUT GUESS: ")
+    wordList = util.createWordList()
+    word = util.pickWord(wordList)
+    secret = util.createSecret(word)
+    badGuessCount = 0
+    newGame = False
 
-    if util.isGuessValid(guess):
-        if util.guessInSecret(secret, guess):
-            badGuessCount += 1
-            print("GUESS ALREADY GUESSED.")
+    while True: # game round loop
+        guess = input("INPUT GUESS: ")
 
-        elif util.guessInWord(word, guess):
-            secret = util.updateSecret(word, secret, guess)
+        if util.isGuessValid(guess):
+            if util.guessInSecret(secret, guess):
+                badGuessCount += 1
+                print("GUESS ALREADY GUESSED.")
+
+            elif util.guessInWord(word, guess):
+                secret = util.updateSecret(word, secret, guess)
+
+            else:
+                badGuessCount += 1
+                print("GUESS BAD.")
+
+            util.printSecret(secret)
+
+            if util.noHyphenInSecret(secret):
+                print("YOU HAVE WON.")
+                
+                while True:
+                        answer = input("PLAY AGAIN? Y/N ")
+                        if answer == "Y":
+                            newGame = True
+                            break
+                        elif answer == "N":
+                            sys.exit()
+                        else:
+                            print("WRONG.")
+            else:
+                util.printGameStatus(badGuessCount)
+                if badGuessCount >= 9:
+                    print("GAME LOST.")
+                    
+                    while True:
+                        answer = input("PLAY AGAIN? Y/N ")
+                        if answer == "Y":
+                            newGame = True
+                            break
+                        elif answer == "N":
+                            sys.exit()
+                        else:
+                            print("WRONG.")
 
         else:
-            badGuessCount += 1
-            print("GUESS BAD.")
+            print("INVALID GUESS. TRY AGAIN.")
 
-        util.printSecret(secret)
-
-        if util.noHyphenInSecret(secret):
-            print("YOU HAVE WON.")
+        if newGame is True:
             break
-        else:
-            util.printGameStatus(badGuessCount)
-            if badGuessCount >= 9:
-                print("GAME LOST.")
-                break
-
-    else:
-        print("INVALID GUESS. TRY AGAIN.")
 
