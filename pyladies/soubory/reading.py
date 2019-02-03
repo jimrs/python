2260931c0880e2a339d0699c40cc83d504170d23
Jiri Mares <marj39@vse.cz>
2019-02-03 22:14:03 +0100

    Completed reading and writing files lesson

---
+++ pyladies/soubory/reading.py
@@ -0,0 +1,8 @@
+print("From the heart, may it again go to the heart!")
+print()
+
+with open("basnicka", encoding="utf-8") as file:
+    for line in file:
+        print(line, end="")
+
+print()
---
+++ pyladies/soubory/writing.py
@@ -0,0 +1,4 @@
+with open("haiku", mode="w", encoding="utf-8") as soubor:
+    print("Úsměv na rtech tvých,", file=soubor)
+    print("prve štěstí přináší,", file=soubor)
+    print("pak smutek navždy.", file=soubor)
